2b59c78697c00cf7e9d9d89e117a61a67bdca6d1
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-03-27 11:19:54 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Foter } from "./Components/Layout/Foter/Foter"
+import { Menu } from "./Components/Layout/Menu/Menu"
+
+function App() {
+
+  return (
+    <>
+      <Menu />
+      <Foter />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Layout/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+export const Menu = () => {
+  return (
+    <>
+      <div className="w-full h-[20%] bg-black">
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
